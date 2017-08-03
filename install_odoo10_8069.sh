870c2652842f5d9d418f697eb4d5b375f51f3070
Josef Kaser <josef.kaser@pragmasoft.de>
2017-08-03 11:42:45 +0200

    new download link for wkhtmltopdf

--- install_odoo10_8069.sh
+++ install_odoo10_8069.sh
@@ -89,7 +89,7 @@ cp $START_DIR/odoo10.conf .
 cd /tmp
 mkdir wkhtmltopdf
 cd wkhtmltopdf
-wget http://download.gna.org/wkhtmltopdf/0.12/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 unxz wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
 tar xvf wkhtmltox-0.12.4_linux-generic-amd64.tar
 cd wkhtmltox/bin
